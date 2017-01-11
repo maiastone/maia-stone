a7b445c0cad12154b0f44cf3abaa2a9955e78c07
Maia Stone <maiadov@gmail.com>
2017-01-10 22:38:20 -0700

    WIP resize screenshots

--- lib/components/ProjectCard.jsx
+++ lib/components/ProjectCard.jsx
@@ -8,9 +8,11 @@ export default class ProjectCard extends Component {
   render() {
     const projectCard = projects.map((project) =>
     <div className='card' key={project.id}>
-      <h2>{project.title}</h2>
-      <img className='screenshot' src={project.imgLink}></img>
-      <p className='description'>{project.description}</p>
+      <div className='card-flex-container'>
+        <img className='screenshot' src={project.imgLink}></img>
+        <h2>{project.title}</h2>
+        <p className='description'>{project.description}</p>
+      </div>
       <ul className='tech'>
           {project.techStack.map(function (tech, i) {
             return <li key={i}>{tech}</li>;
